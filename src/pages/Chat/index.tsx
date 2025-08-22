49e6acf533df8a357d4922b2e505b8edabb653ee
Jul.Nazz <63221845+kingxjullien14@users.noreply.github.com>
2025-08-22 15:32:45 +0800

    Update message usage for enterprise plan

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -3467,7 +3467,7 @@ console.log(baseUrl);
 
       // Set message usage for enterprise plan
       if (data.companyData.plan === "enterprise") {
-        // setMessageUsage(data.messageUsage);
+        setMessageUsage(data.messageUsage.aiMessages);
       }
 
       // Set employee list
